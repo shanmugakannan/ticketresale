0a1fef24ae7119364ce56e40a215e26af0aae195
shanmugakannan <shanmugakannan@gmail.com>
2015-06-02 12:40:03 +0530

    Create server.js
    app entry point

---
+++ server.js
@@ -0,0 +1,25 @@
+// server.js
+
+// modules =================================================
+var express        = require('express');
+var app            = express();
+
+// configuration ===========================================
+
+// set our port
+var port = process.env.PORT || 8080; 
+
+// set the static files location /public/img will be /img 
+app.use(express.static(__dirname + '/public')); 
+
+// routes ==================================================
+require('./app/routes')(app); // configure our routes
+
+// start app ===============================================
+app.listen(port);               
+
+// Console.log                  
+console.log('Express runs on ' + port);
+
+// expose app           
+exports = module.exports = app;
