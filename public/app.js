09ee4f811e675a1712bb17340a08c1e4ac78c9c7
shanmugakannan <shanmugakannan@gmail.com>
2015-06-02 14:10:43 +0530

    Delete app.js

--- public/app.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  var app = angular.module("app", []);
-
-  app.controller('MainController', function(){
-    this.products = [{name:'ticket1'},{name:'ticket2'}];
-    this.title = "ticket";
-  });
- })();
