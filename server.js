70dbe5c7d0044a00ddedcf8a88bb672210debc04
Shanmugakannan <shanmugakannan@gmail.com>
2015-06-10 08:40:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,13 +3,7 @@
 // modules =================================================
 var express        = require('express');
 var app            = express();
-var mongoose = require('mongoose');    
-
-//Logging =================================================
-var logentries = require('node-logentries');
-var log = logentries.logger({
-  token:'2fc29b03-5bc8-4bbf-8b50-da90a60e9bfe'
-});
+var mongoose = require('mongoose');
 
 // configuration ===========================================
 var db = require('./app/config/db');
@@ -19,7 +13,6 @@ var port = process.env.PORT || 8080;
 
 //Connect to database
 mongoose.connect(db.url);
-log.info("Connected to MongoDB");
 
 // set the static files location /public/img will be /img 
 app.use(express.static(__dirname + '/public/modules/ticket/')); 
@@ -32,6 +25,5 @@ app.listen(port);
 
 // Console.log                  
 console.log('Ticket Resale runs on ' + port);
-log.info('Ticket Resale runs on ' + port");
 // expose app           
 exports = module.exports = app;
