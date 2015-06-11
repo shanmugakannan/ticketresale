61f5b3c18f7109248bda71749a93bcbee3946c30
shanmugakannan <shanmugakannan@gmail.com>
2015-06-11 08:42:36 +0530

    Angular routes

--- public/modules/ticket/controllers/ticket.client.controller.js
+++ public/modules/ticket/controllers/ticket.client.controller.js
@@ -1,5 +1,5 @@
 (function() {
-  var app = angular.module("app", []);
+  var app = angular.module("app", ['ngAnimate', 'ui.router']);
 
   app.controller('TicketController', function(){
     this.products = [{name:'ticket1'},{name:'ticket2'}];
@@ -7,4 +7,57 @@
     this.input1 = "1"; 
     this.input2 = "2";
   });
+  
+  
+  app.config(function($stateProvider, $urlRouterProvider) {
+    
+    $stateProvider
+    
+        //  /formcontainer 
+        .state('ticket', {
+            url: '/ticket',
+            templateUrl: './public/modules/ticket/views/formcontainer.html',
+            controller: 'formController'
+        })
+        
+  
+        //  /tickets/choose 
+        .state('ticket.choose', {
+            url: '/choose',
+            templateUrl: 'form-choose.html'
+        })
+        
+        // url will be /form/interests
+        .state('ticket.confirm', {
+            url: '/confirm',
+            templateUrl: 'form-confirm.html'
+        })
+        
+        // url will be /form/payment
+        .state('ticket.payment', {
+            url: '/payment',
+            templateUrl: 'form-payment.html'
+        });
+       
+    // catch all route
+    // send users to the form page 
+    $urlRouterProvider.otherwise('/form/ticket');
+})
+
+// our controller for the form
+// =============================================================================
+app.controller('formController', function($scope) {
+    
+    // we will store all of our form data in this object
+    $scope.formData = {};
+    
+    // function to process the form
+    $scope.processForm = function() {
+        alert('awesome!');  
+    };
+    
+});
+
+  
+  
  })();
